c5b8132df9dd21f98b9638ba655c534ff860ef10
Anton Punith <antonpunith@gmail.com>
2020-02-11 11:07:04 +1100

    prettified content

--- app/components/Resume/Index.jsx
+++ app/components/Resume/Index.jsx
@@ -1,53 +1,48 @@
-import React from 'react';
-import Header from './components/Header';
-import Headline from './components/Headline';
-import Objective from './components/Objective';
-import Summary from './components/Summary';
-import Skills from './components/Skills';
-import Projects from './components/Projects';
-import Work from './components/Work';
-import Education from './components/Education';
-import GoogleTagManager from '../GoogleTagManager';
-var request = require('superagent');
+import React from "react";
+import Header from "./components/Header";
+import Headline from "./components/Headline";
+import Objective from "./components/Objective";
+import Summary from "./components/Summary";
+import Skills from "./components/Skills";
+import Projects from "./components/Projects";
+import Work from "./components/Work";
+import Education from "./components/Education";
+import GoogleTagManager from "../GoogleTagManager";
+var request = require("superagent");
 
 export default React.createClass({
+  displayName: "Resume",
 
-	displayName: 'Resume',
+  getInitialState() {
+    return { data: null };
+  },
 
-	getInitialState () {
-		return { data: null };
-	},
+  componentDidMount() {
+    var Resume = this;
+    request.get(this.props.url).end(function(err, res) {
+      Resume.setState({ data: JSON.parse(res.text) });
+    });
+  },
 
-	componentDidMount () {
-		var Resume = this;
-		request.get(this.props.url)
-			.end(function(err,res) {
-				Resume.setState({'data':JSON.parse(res.text)});
-			});
-	},
+  render() {
+    let { data } = this.state;
 
+    if (!data) {
+      return null;
+    }
 
-	render () {
-
-		let { data } = this.state;
-
-		if (!data) {
-			return null;
-		}
-
-		return (
-			<div>
-				<Header basics={data.basics} />
-				<Headline headlines={data.headlines} />
-				<Objective objectives={data.objectives} />
-				<Summary summary={data.summary} />
-				<Education education={data.education} />
-				<Skills skills={data.skills} />
-				<Work work={data.work} />
-				<Projects projects={data.projects} />
-				<GoogleTagManager gtmId='GTM-TN2FKV' />
-			</div>
-		);
-	}
-
+    return (
+      <div>
+        <Header basics={data.basics} />
+        <Headline headlines={data.headlines} />
+        <Objective objectives={data.objectives} />
+        <Summary summary={data.summary} />
+        <Education education={data.education} />
+        <Skills skills={data.skills} />
+        <Work work={data.work} />
+        <Projects projects={data.projects} />
+        <GoogleTagManager gtmId="GTM-TN2FKV" />
+      </div>
+    );
+  }
 });
--- app/index.jsx
+++ app/index.jsx
@@ -1,8 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Resume from './components/Resume';
+import React from "react";
+import ReactDOM from "react-dom";
+import Resume from "./components/Resume";
 
 ReactDOM.render(
   <Resume url="https://antonpunith.github.io/data/resume.json" />,
-  document.getElementById('app')
+  document.getElementById("app")
 );
