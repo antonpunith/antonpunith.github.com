f2a7b026b090cd2922c40c0b5666c4288c9b61e8
Anton Punith <anton@ozzi.tech>
2023-02-21 12:46:41 +1100

    wip

---
+++ src/components/Resume/components/Headline/index.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import moment from 'moment';
+
+export default React.createClass({
+	displayName: 'Summary',
+
+	renderHeadlines (headline, key) {
+		if (!headline.active) {
+			return null;
+		}
+		return (
+			<div key={key}>
+				{headline.position} with over {moment().diff(headline.careerStart,'years')} years of experience in {headline.skill}.
+			</div>
+		);
+	},
+
+	render () {
+		const { headlines } = this.props;
+		return (
+			<h3>
+				{headlines.map(this.renderHeadlines)}
+			</h3>
+		);
+	}
+});
